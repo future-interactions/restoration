f6c9db2d4ef51f3efaee14ef630e6814e7960172
future-interactions <matt@future-interactions.com>
2022-06-21 20:01:15 +0100

    Added soft edges to brush and move detection

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 let oldImgSource, newImgSource, oldImg, newImg;
 var colList = [];
-var cols = 10;
-var rows = 10;
+var cols = 30;
+var rows = 30;
 var clicks = 0;
 let px, py;
 let sampleList = [23, 25, 90];
+let moved=false;
 
 function preload() {
 	oldImgSource = loadImage('assets/before_artemesia2.png');
@@ -22,20 +23,20 @@ function setup() {
 }
 
 function draw() {
-	background(220,220,205);
-	image(oldImgSource, width/2-oldImgSource.width/2,height/2-oldImgSource.height/2);
+	background(220, 220, 205);
+	image(oldImgSource, width / 2 - oldImgSource.width / 2, height / 2 - oldImgSource.height / 2);
 
-	px = mouseX-width/2+oldImgSource.width/2;
-	py = mouseY-height/2+oldImgSource.height/2;
+	px = mouseX - width / 2 + oldImgSource.width / 2;
+	py = mouseY - height / 2 + oldImgSource.height / 2;
 	for (let i = 0; i < cols; i++) {
-			for (let j = 0; j < rows; j++) {
+		for (let j = 0; j < rows; j++) {
 
-		var c = px;
-		//console.log(px);
-		colList[i][j] = newImgSource.get(px+i-cols/2, py+j-rows/2);
+			var c = px;
+			//console.log(px);
+			colList[i][j] = newImgSource.get(px + i - cols / 2, py + j - rows / 2);
 
-		//colList[i][j] = mouseX+j+i*rows;
-			}
+			//colList[i][j] = mouseX+j+i*rows;
+		}
 	}
 	/*
 	let colAlpha = newImgSource.get(mouseX, mouseY);
@@ -45,24 +46,29 @@ function draw() {
 	image(oldImgSource, 0, 0);
 	image(newImg, 0, 0);
 	*/
+	if (moved=true){
 	for (let i = 0; i < cols; i++) {
 		for (let j = 0; j < cols; j++) {
-		newImg.noStroke();
-		let currentCol=color(colList[i][j]);
-		currentCol.setAlpha(20);
-		newImg.fill(currentCol);
-		newImg.rect(px+i-cols/2, py+j-rows/2, 1, 1);
+			let d = dist(i-cols/2, j-rows/2, px, py);
+			console.log(d)
+			newImg.noStroke();
+			let currentCol = color(colList[i][j]);
+			currentCol.setAlpha(255 - (d/2.25));
+			newImg.fill(currentCol);
+			newImg.rect(px + i - cols / 2, py + j - rows / 2, 1, 1);
+		}
 	}
+	moved=false;
 }
-translate(width/2-oldImgSource.width/2,height/2-oldImgSource.height/2)
+	translate(width / 2 - oldImgSource.width / 2, height / 2 - oldImgSource.height / 2)
 	image(newImg, 0, 0);
 }
 
 function make2DArray(cols, rows) {
 	var arr = new Array(cols);
-		for (let i = 0; i < arr.length; i++) {
-			arr[i] = new Array(rows);
-		}
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = new Array(rows);
+	}
 	return arr;
 }
 
@@ -73,6 +79,10 @@ function mousePressed() {
 	console.log(sampleList);
 }
 
-function touchMoved(){
-  return false;
+function touchMoved() {
+	return false;
+}
+
+function mouseMoved() {
+	moved = true;
 }
